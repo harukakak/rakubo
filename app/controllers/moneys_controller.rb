5230f5b5cad9a03aa9b95533b125efd38b016135
harukakak <87980271+harukakak@users.noreply.github.com>
2022-02-14 17:22:03 +0900

    細かいビューの調整1

--- app/controllers/moneys_controller.rb
+++ app/controllers/moneys_controller.rb
@@ -1,6 +1,10 @@
 class MoneysController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :show, :update]
   before_action :set, only: [:show, :edit, :update, :destroy]
+  before_action :flg_init, only: [:index]
+  
+  $lastmonthflg = 0
+  $userid = ""
 
   def set
     @money = Money.find(params[:id])
@@ -12,6 +16,60 @@ class MoneysController < ApplicationController
 
   def new
     @money = Money.new
+    if $lastmonthflg == 1
+      @lastmoney = lastmonthdatasearch()
+      if @lastmoney.present?
+      @money = Money.new(
+        month: @lastmoney.month,
+        income_account_name: @lastmoney.income_account_name,
+        payment_account1_name: @lastmoney.payment_account1_name,
+        income_money: @lastmoney.income_money,
+        payment1_money: @lastmoney.payment1_money,
+        item_name1_id: @lastmoney.item_name1_id,
+        item_name2_id: @lastmoney.item_name2_id,
+        item_name3_id: @lastmoney.item_name3_id,
+        item_name4_id: @lastmoney.item_name4_id,
+        item_name5_id: @lastmoney.item_name5_id,
+        item_name6_id: @lastmoney.item_name6_id,
+        item_name7_id: @lastmoney.item_name7_id,
+        item_name8_id: @lastmoney.item_name8_id,
+        payment_account2_name: @lastmoney.payment_account2_name,
+        payment_account3_name: @lastmoney.payment_account3_name,
+        payment2_money: @lastmoney.payment2_money,
+        payment3_money: @lastmoney.payment3_money,
+        payment4_money: @lastmoney.payment4_money,
+        payment5_money: @lastmoney.payment5_money,
+        payment6_money: @lastmoney.payment6_money,
+        payment7_money: @lastmoney.payment7_money,
+        payment8_money: @lastmoney.payment8_money,
+        payment9_money: @lastmoney.payment9_money,
+        payment10_money: @lastmoney.payment10_money,
+        payment11_money: @lastmoney.payment11_money,
+        item_name1_self: @lastmoney.item_name1_self,
+        item_name2_self: @lastmoney.item_name2_self,
+        item_name3_self: @lastmoney.item_name3_self,
+        income_account2_name: @lastmoney.income_account2_name,
+        income2_money: @lastmoney.income2_money,
+        user_id: @lastmoney.user_id
+      )
+    end
+    end
+  end
+
+  def flg_init
+    $lastmonthflg = 0
+    $userid = ""
+  end
+
+  def lastmonthdatasearch()
+    @moneys = Money.where(user_id: $userid).order("created_at DESC").limit(1)
+    return @moneys.first
+  end
+
+  def lastmonthsearch
+    $lastmonthflg = 1
+    $userid = params[:id]
+    redirect_to new_money_path
   end
 
   def create
@@ -38,6 +96,7 @@ class MoneysController < ApplicationController
   end
 
   def destroy
+    @money = Money.find(params[:id])
     if current_user.id == @money.user_id
       @money.destroy
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'moneys/index'
   root to: "moneys#index"
+  resources :moneys do
+    get 'lastmonthsearch', on: :member
+  end
   resources :users
-  resources :moneys
   resources :memos
 end
