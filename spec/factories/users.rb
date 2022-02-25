fcb5aecb990e2eb8e7d8828e3ef719f11aa3b48c
harukakak <87980271+harukakak@users.noreply.github.com>
2022-02-25 12:47:57 +0900

    ユーザー機能の結合テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
 
     nick_name             { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password              { Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
   end
 end
